97b33e6a93e77bdd4060ac5b18402e24f110c200
Nick Edwards <nledwa01@gmail.com>
2018-09-27 17:30:45 -0400

    Added list of books and ability to loop thru them

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,5 +1,8 @@
 <template>
     <h1>{{title}}</h1>
+        <ul>
+            <li v-for="book in books">{{book.title}}: {{book.author}}</li>
+        </ul>
 </template>
 
 <script>
@@ -7,7 +10,12 @@ export default {
     name: BookList,
     data () {
         return {
-            title: "All Books"
+            title: "All Books",
+            books:  [
+                        {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+                        {title: 'American Gods', author: 'Neil Gaiman'},
+                        {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+                    ]
         };
     },
 }
@@ -16,5 +24,15 @@ export default {
 <style>
 h1, h2 {
     font-weight: normal;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: block;
+    margin: 0 10px;
 }
 </style>
